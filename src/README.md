f6c2f366aaf748f83f72fd1ba928f7f7ba2c20c3
bergi <s.berke@netzkolchose.de>
2023-02-18 21:50:20 +0100

    add the st3 `TECH.DOC` and start understanding the patterns to eventually calculate the run-time of the songs

---
+++ src/README.md
@@ -0,0 +1,6 @@
+Some python code for reading the s3m files.
+
+- [TECH.DOC](TECH.DOC): The *official* screamtracker 3.2 file format documentation
+- [s3m.py](s3m.py): s3m reader based on [jangler/s3mml](https://github.com/jangler/s3mml)
+- [render.py](render.py): updates the README and the website index
+
--- src/s3m.py
+++ src/s3m.py
@@ -4,66 +4,108 @@ Based on https://github.com/jangler/s3mml
 import struct
 import dataclasses
 from pathlib import Path
-from typing import List, IO, Union, Optional
+from typing import IO, Union, Optional, Tuple, List, Generator
 
 
 NOTE_NAMES = (
-    'C-',
-    'C#',
-    'D-',
-    'D#',
-    'E-',
-    'F-',
-    'F#',
-    'G-',
-    'G#',
-    'A-',
-    'A#',
-    'B-',
+    'C-', 'C#', 'D-', 'D#', 'E-', 'F-', 'F#', 'G-', 'G#', 'A-', 'A#', 'B-',
 )
+
+
+@dataclasses.dataclass
+class Header:
+    filename: Optional[str] = None
+    title: str = ""
+    numorders: int = 0
+    numinstruments: int = 0
+    numpatterns: int = 0
+    flags: int = 0
+    trackerversion: int = 0
+    sampletype: int = 0
+    globalvolume: int = 0
+    initialspeed: int = 0
+    initialtempo: int = 0
+    mastervolume: int = 0
+    ultraclickremoval: int = 0
+    defaultpan: int = 0
+    channelsettings: Tuple[int, ...] = tuple()
+    orderlist: Tuple[int, ...] = tuple()
+    ptrinstruments: List[int] = tuple()
+    ptrpatterns: List[int] = tuple()
+
+
+@dataclasses.dataclass
+class Instrument:
+    type: int = 0
+    title: str = ""
+    filename: str = ""
+    length: Optional[int] = None
+    loop_start: Optional[int] = None
+    loop_end: Optional[int] = None
+    volume: int = 0
+
+
+@dataclasses.dataclass
+class Operator:
+    tremolo: int = 0
+    vibrato: int = 0
+    sustainsound: int = 0
+    scaleenv: int = 0
+    freqmult: int = 0
+    levelscaling: int = 0
+    volume: int = 0
+    attack: int = 0
+    decay: int = 0
+    sustain: int = 0
+    release: int = 0
+    waveselect: int = 0
+
+
+@dataclasses.dataclass
+class ChannelRow:
+    note: Optional[int] = None
+    instrument: Optional[int] = None
+    volume: Optional[int] = None
+    effect: Optional[int] = None
+    param: Optional[int] = None
+
+
+PatternType = List[List[Optional[ChannelRow]]]
 
 
 class S3m:
 
     character_encoding = "CP437"
 
-    class Header:
-        pass
-
-    @dataclasses.dataclass
-    class Instrument:
-        type: int = 0
-        title: str = ""
-        filename: str = ""
-        length: Optional[int] = None
-        loop_start: Optional[int] = None
-        loop_end: Optional[int] = None
-        volume: int = 0
-
-    class Operator:
-        pass
-
     def __init__(self):
-        self.header = self.Header()
-        self.instruments: List[self.Instrument] = []
-        self.patterns: List = []
+        self.header = Header()
+        self.instruments: List[Instrument] = []
+        self.patterns: List[ChannelRow] = []
 
     @classmethod
     def from_bytes(cls, buf: bytes) -> "S3m":
         s3m = cls()
         s3m._read_header(buf)
         s3m._read_instruments(buf)
-        #s3m._read_patterns(buf)
+        s3m._read_patterns(buf)
         return s3m
 
     @classmethod
     def from_file(cls, name: Union[str, Path]) -> "S3m":
-        return cls.from_bytes(Path(name).read_bytes())
+        m = cls.from_bytes(Path(name).read_bytes())
+        m.header.filename = str(name)
+        return m
 
     def dump(self, file: Optional[IO[str]] = None):
         print(self.header.title, file=file)
         for inst in self.instruments:
             print(f"  {inst.type} {inst.title:28} {inst.volume or 0:2} {inst.length or 0:5}", file=file)
+
+    def iter_patterns(self) -> Generator[Tuple[int, PatternType], None, None]:
+        for pattern_idx in self.header.orderlist:
+            if pattern_idx == 255:
+                break
+            yield pattern_idx, self.patterns[pattern_idx]
 
     def _to_string(self, b: bytes) -> str:
         return b.decode(self.character_encoding).strip("\0")
@@ -93,7 +135,7 @@ class S3m:
         """Read instrument information from buf into m."""
         self.instruments = []
         for ptr in self.header.ptrinstruments:
-            inst = self.Instrument()
+            inst = Instrument()
             inst.type = struct.unpack_from('B', buf, ptr)[0]
             inst.filename = self._to_string(struct.unpack_from('12s', buf, ptr + 1)[0])
 
@@ -119,7 +161,7 @@ class S3m:
                 oplvalues = struct.unpack_from('<12B', buf, ptr + 16)
                 inst.feedback = oplvalues[10] >> 1
                 inst.connection = oplvalues[10] & 1 != 0
-                inst.carrier, inst.modulator = self.Operator(), self.Operator()
+                inst.carrier, inst.modulator = Operator(), Operator()
                 for offset, op in enumerate([inst.modulator, inst.carrier]):
                     op.tremolo = oplvalues[offset] & 0x80 != 0
                     op.vibrato = oplvalues[offset] & 0x40 != 0
@@ -145,9 +187,13 @@ class S3m:
             pat = []
             ptr += 2  # skip packed length bytes
             for i in range(64):
-                row = [None for j in range(32)]
+                row: List[Optional[ChannelRow]] = [None for _ in range(32)]
                 while True:
-                    what = struct.unpack_from('B', buf, ptr)[0]
+                    try:
+                        what = struct.unpack_from('B', buf, ptr)[0]
+                    except struct.error:
+                        print(f"READ ERROR: ptr={ptr} len(buf)={len(buf)} patterns={len(self.patterns)}")
+                        return
                     ptr += 1
                     if what == 0:
                         break
@@ -161,6 +207,8 @@ class S3m:
                     if what & 0x80:  # effect and parameter(s)
                         data[3], data[4] = struct.unpack_from('BB', buf, ptr)
                         ptr += 2
-                    row[what & 0x1f] = data
+                    row[what & 0x1f] = ChannelRow(*data)
+                    
                 pat.append(row)
+
             self.patterns.append(pat)
